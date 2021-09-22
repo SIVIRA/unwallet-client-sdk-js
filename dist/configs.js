71bf8fb7efad24453836ddfd8329ff2a6c956f72
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-09-22 15:46:43 +0900

    target: es5 -> es6

--- dist/configs.js
+++ dist/configs.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var configs = {
+const configs = {
     prod: {
         clientID: "",
         dAuth: {
--- dist/dauth.js
+++ dist/dauth.js
@@ -4,31 +4,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DAuth = void 0;
-var configs_1 = __importDefault(require("./configs"));
-var DAuth = /** @class */ (function () {
-    function DAuth(config, ws) {
+const configs_1 = __importDefault(require("./configs"));
+class DAuth {
+    constructor(config, ws) {
         this.config = config;
         this.ws = ws;
         this.connectionID = "";
-        this.resolve = function (result) { };
-        this.reject = function (reason) { };
+        this.resolve = (result) => { };
+        this.reject = (reason) => { };
     }
-    DAuth.init = function (args) {
-        return new Promise(function (resolve, reject) {
+    static init(args) {
+        return new Promise((resolve, reject) => {
             if (!args.env) {
                 args.env = "prod";
             }
             if (!(args.env in configs_1.default)) {
                 throw Error("invalid env");
             }
-            var config = configs_1.default[args.env];
+            const config = configs_1.default[args.env];
             config.clientID = args.clientID;
-            var dAuth = new DAuth(config, new WebSocket(config.dAuth.wsAPIURL));
-            dAuth.ws.onerror = function (event) {
+            const dAuth = new DAuth(config, new WebSocket(config.dAuth.wsAPIURL));
+            dAuth.ws.onerror = (event) => {
                 reject("websocket connection failed");
             };
-            dAuth.ws.onmessage = function (event) {
-                var msg = JSON.parse(event.data);
+            dAuth.ws.onmessage = (event) => {
+                const msg = JSON.parse(event.data);
                 switch (msg.type) {
                     case "connectionID":
                         dAuth.connectionID = msg.data.value;
@@ -63,20 +63,20 @@ var DAuth = /** @class */ (function () {
                         break;
                 }
             };
-            dAuth.ws.onopen = function (event) {
+            dAuth.ws.onopen = (event) => {
                 dAuth.getConnectionID(dAuth.ws);
             };
         });
-    };
-    DAuth.prototype.initPromiseArgs = function () {
-        this.resolve = function (result) { };
-        this.reject = function (reason) { };
-    };
-    DAuth.prototype.authorize = function (args) {
+    }
+    initPromiseArgs() {
+        this.resolve = (result) => { };
+        this.reject = (reason) => { };
+    }
+    authorize(args) {
         if (!args.responseMode) {
             args.responseMode = "fragment";
         }
-        var url = new URL(this.config.dAuth.authURL);
+        const url = new URL(this.config.dAuth.authURL);
         url.searchParams.set("response_type", "id_token");
         url.searchParams.set("response_mode", args.responseMode);
         url.searchParams.set("client_id", this.config.clientID);
@@ -84,58 +84,54 @@ var DAuth = /** @class */ (function () {
         url.searchParams.set("redirect_uri", args.redirectURL);
         url.searchParams.set("nonce", args.nonce);
         location.assign(url.toString());
-    };
-    DAuth.prototype.sign = function (args) {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.resolve = resolve;
-            _this.reject = reject;
-            var url = new URL(_this.config.dAuth.baseURL + "/x/sign");
-            url.searchParams.set("connectionID", _this.connectionID);
+    }
+    sign(args) {
+        return new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+            const url = new URL(`${this.config.dAuth.baseURL}/x/sign`);
+            url.searchParams.set("connectionID", this.connectionID);
             url.searchParams.set("message", args.message);
-            _this.openWindow(url);
+            this.openWindow(url);
         });
-    };
-    DAuth.prototype.signAssetTransfer = function (args) {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.resolve = resolve;
-            _this.reject = reject;
-            var url = new URL(_this.config.dAuth.baseURL + "/x/signAssetTransfer");
-            url.searchParams.set("connectionID", _this.connectionID);
+    }
+    signAssetTransfer(args) {
+        return new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+            const url = new URL(`${this.config.dAuth.baseURL}/x/signAssetTransfer`);
+            url.searchParams.set("connectionID", this.connectionID);
             url.searchParams.set("id", args.id.toString());
             url.searchParams.set("to", args.to);
             url.searchParams.set("amount", args.amount.toString());
-            _this.openWindow(url);
+            this.openWindow(url);
         });
-    };
-    DAuth.prototype.createPresentation = function (args) {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.resolve = resolve;
-            _this.reject = reject;
-            var url = new URL(_this.config.dAuth.baseURL + "/x/createPresentation");
-            url.searchParams.set("connectionID", _this.connectionID);
+    }
+    createPresentation(args) {
+        return new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+            const url = new URL(`${this.config.dAuth.baseURL}/x/createPresentation`);
+            url.searchParams.set("connectionID", this.connectionID);
             url.searchParams.set("credential", args.credential);
             url.searchParams.set("challenge", args.challenge);
-            _this.openWindow(url);
+            this.openWindow(url);
         });
-    };
-    DAuth.prototype.getConnectionID = function (ws) {
+    }
+    getConnectionID(ws) {
         this.sendWSMessage(ws, {
             action: "getConnectionID",
         });
-    };
-    DAuth.prototype.sendWSMessage = function (ws, message) {
+    }
+    sendWSMessage(ws, message) {
         ws.send(JSON.stringify(message));
-    };
-    DAuth.prototype.openWindow = function (url) {
-        var width = 480;
-        var height = 480;
-        var left = (screen.width - width) / 2;
-        var top = (screen.height - height) / 2;
-        window.open(url.toString(), "_blank", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
-    };
-    return DAuth;
-}());
+    }
+    openWindow(url) {
+        const width = 480;
+        const height = 480;
+        const left = (screen.width - width) / 2;
+        const top = (screen.height - height) / 2;
+        window.open(url.toString(), "_blank", `width=${width},height=${height},left=${left},top=${top}`);
+    }
+}
 exports.DAuth = DAuth;
