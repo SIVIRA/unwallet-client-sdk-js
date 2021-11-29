5127d78993fa5d69460c55c138ddf5b8c2d8ba7c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-11-29 19:55:51 +0900

    update wsAPIURL

--- src/configs.ts
+++ src/configs.ts
@@ -4,16 +4,16 @@ export const unWalletConfigs: { [env: string]: UnWalletConfig } = {
   prod: {
     baseURL: "https://id.unwallet.world",
     authURL: "https://id.unwallet.world/authorize",
-    wsAPIURL: "wss://ws-api.admin.id.dauth.world",
+    wsAPIURL: "wss://in-ws-api.id.unwallet.world",
   },
   dev: {
     baseURL: "https://id.unwallet.dev",
     authURL: "https://id.unwallet.dev/authorize",
-    wsAPIURL: "wss://ws-api.admin.id-dev.dauth.world",
+    wsAPIURL: "wss://in-ws-api.id.unwallet.dev",
   },
   local: {
     baseURL: "http://localhost:4200",
     authURL: "http://localhost:4200/authorize",
-    wsAPIURL: "wss://ws-api.admin.id-dev.dauth.world",
+    wsAPIURL: "wss://in-ws-api.id.unwallet.dev",
   },
 };
