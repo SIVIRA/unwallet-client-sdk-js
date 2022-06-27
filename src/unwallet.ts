2962ea6ccf3a5f7b0697b65ccb72d5f53fc84c43
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2022-06-27 21:18:12 +0900

    break

--- src/unwallet.ts
+++ src/unwallet.ts
@@ -188,6 +188,7 @@ export class UnWallet {
           default:
             throw new Error(msg.value);
         }
+        break;
 
       default:
         throw new Error(`unknown message type: ${msg.type}`);
