451f7d45bb22de6574840f7a4dadd4f5c4783605
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-08-04 03:25:46 +0900

    test: init x connection

--- src/x.test.ts
+++ src/x.test.ts
@@ -8,8 +8,9 @@ import {
 } from "vitest";
 
 import { getUnWalletConfigByEnv } from "./config";
-import { mockXAPI } from "./testutil";
-import { XAction, XConnection } from "./x";
+import { UWError } from "./error";
+import { XActionMockArgs, mockXAPI } from "./testutil";
+import { XAction, XConnection, XResponse } from "./x";
 
 const uwConfig = getUnWalletConfigByEnv("dev");
 
@@ -18,6 +19,8 @@ const xConnID = "xconn";
 let xActionToCallCount: Record<XAction, number> = {
   getConnectionID: 0,
 };
+
+let handleGetConnectionID: (args: XActionMockArgs) => void = () => {};
 
 const xAPIMock = mockXAPI({
   config: uwConfig.xAPI,
@@ -28,14 +31,7 @@ const xAPIMock = mockXAPI({
         break;
     }
   },
-  handleGetConnectionID: ({ client }) => {
-    client.send(
-      JSON.stringify({
-        type: "connectionID",
-        value: xConnID,
-      }),
-    );
-  },
+  handleGetConnectionID: (args) => handleGetConnectionID(args),
 });
 
 beforeAll(() =>
@@ -53,9 +49,38 @@ afterAll(() => xAPIMock.server.close());
 
 describe("XConnection.init", () => {
   test("success", async () => {
+    handleGetConnectionID = ({ client }) => {
+      client.send(
+        JSON.stringify({
+          type: "connectionID",
+          value: xConnID,
+        } satisfies XResponse),
+      );
+    };
+
     const xConn = await XConnection.init(uwConfig.xAPI);
     expect(xConn.id).toBe(xConnID);
     expect(xConn.readyState).toBe(WebSocket.OPEN);
+
+    expect(xActionToCallCount.getConnectionID).toBe(1);
+  });
+
+  test("failure: invalid response: unexpected type", async () => {
+    handleGetConnectionID = ({ client }) => {
+      client.send(
+        JSON.stringify({
+          type: "error",
+          value: "something went wrong",
+        } satisfies XResponse),
+      );
+    };
+
+    await expect(XConnection.init(uwConfig.xAPI)).rejects.toThrow(
+      new UWError(
+        "INVALID_RESPONSE",
+        "unexpected type: error (value: something went wrong)",
+      ),
+    );
 
     expect(xActionToCallCount.getConnectionID).toBe(1);
   });
