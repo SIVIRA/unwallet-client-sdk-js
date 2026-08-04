49b22fc0f604fbb85b615c33d916647319de6c81
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-08-04 13:34:57 +0900

    refactor: test for x connection

--- src/x.test.ts
+++ src/x.test.ts
@@ -1,11 +1,4 @@
-import {
-  afterAll,
-  beforeAll,
-  beforeEach,
-  describe,
-  expect,
-  test,
-} from "vitest";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
 
 import { UWError } from "./error";
 import { XAPIMockHandlers, mockXAPI } from "./testutil";
@@ -50,102 +43,109 @@ beforeEach(() => {
 afterAll(() => xAPIMock.server.close());
 
 describe("XConnection", () => {
-  test("success: init", async () => {
-    xAPIMockHandlers.getConnectionID = (args) => {
-      args.client.send(
-        JSON.stringify({
-          type: "connectionID",
-          value: xConnID,
-        } satisfies XResponse),
-      );
-    };
+  describe("init", () => {
+    it("resolves", async () => {
+      xAPIMockHandlers.getConnectionID = (args) => {
+        args.client.send(
+          JSON.stringify({
+            type: "connectionID",
+            value: xConnID,
+          } satisfies XResponse),
+        );
+      };
 
-    const xConn = await XConnection.init({
-      url: xAPIURL,
-      connectionTimeout: 1_000,
+      const xConn = await XConnection.init({
+        url: xAPIURL,
+        connectionTimeout: 1_000,
+      });
+      expect(xConn.id).toBe(xConnID);
+      expect(xConn.readyState).toBe(WebSocket.OPEN);
+
+      expect(xActionToCallCount.getConnectionID).toBe(1);
     });
-    expect(xConn.id).toBe(xConnID);
-    expect(xConn.readyState).toBe(WebSocket.OPEN);
 
-    expect(xActionToCallCount.getConnectionID).toBe(1);
-  });
+    it("rejects on invalid response: invalid payload", async () => {
+      let isXConnectionClosed = false;
 
-  test("failure: init: invalid response: invalid json string", async () => {
-    let isXConnectionClosed = false;
+      xAPIMockHandlers.getConnectionID = (args) =>
+        args.client.send("invalid json string");
+      xAPIMockHandlers.onConnectionClosed = () => (isXConnectionClosed = true);
 
-    xAPIMockHandlers.getConnectionID = (args) =>
-      args.client.send("invalid json string");
-    xAPIMockHandlers.onConnectionClosed = () => (isXConnectionClosed = true);
-
-    await expect(
-      XConnection.init({
-        url: xAPIURL,
-        connectionTimeout: 1_000,
-      }),
-    ).rejects.toThrow(
-      new UWError("INVALID_RESPONSE", "invalid payload: ✖ Invalid JSON string"),
-    );
-
-    expect(xActionToCallCount.getConnectionID).toBe(1);
-    expect(isXConnectionClosed).toBe(true);
-  });
-
-  test("failure: init: invalid response: unexpected type", async () => {
-    let isXConnectionClosed = false;
-
-    xAPIMockHandlers.getConnectionID = (args) => {
-      args.client.send(
-        JSON.stringify({
-          type: "error",
-          value: "something went wrong",
-        } satisfies XResponse),
+      await expect(
+        XConnection.init({
+          url: xAPIURL,
+          connectionTimeout: 1_000,
+        }),
+      ).rejects.toThrow(
+        new UWError(
+          "INVALID_RESPONSE",
+          "invalid payload: ✖ Invalid JSON string",
+        ),
       );
-    };
-    xAPIMockHandlers.onConnectionClosed = () => (isXConnectionClosed = true);
 
-    await expect(
-      XConnection.init({
-        url: xAPIURL,
-        connectionTimeout: 1_000,
-      }),
-    ).rejects.toThrow(
-      new UWError(
-        "INVALID_RESPONSE",
-        "unexpected type: error (value: something went wrong)",
-      ),
-    );
+      expect(xActionToCallCount.getConnectionID).toBe(1);
+      expect(isXConnectionClosed).toBe(true);
+    });
 
-    expect(xActionToCallCount.getConnectionID).toBe(1);
-    expect(isXConnectionClosed).toBe(true);
-  });
+    it("rejects on invalid response: unexpected type", async () => {
+      let isXConnectionClosed = false;
 
-  test("failure: init: connection timeout", async () => {
-    let isXConnectionClosed = false;
+      xAPIMockHandlers.getConnectionID = (args) => {
+        args.client.send(
+          JSON.stringify({
+            type: "error",
+            value: "something went wrong",
+          } satisfies XResponse),
+        );
+      };
+      xAPIMockHandlers.onConnectionClosed = () => (isXConnectionClosed = true);
 
-    xAPIMockHandlers.onConnectionClosed = () => (isXConnectionClosed = true);
+      await expect(
+        XConnection.init({
+          url: xAPIURL,
+          connectionTimeout: 1_000,
+        }),
+      ).rejects.toThrow(
+        new UWError(
+          "INVALID_RESPONSE",
+          "unexpected type: error (value: something went wrong)",
+        ),
+      );
 
-    await expect(
-      XConnection.init({
-        url: xAPIURL,
-        connectionTimeout: 100,
-      }),
-    ).rejects.toThrow(new UWError("CONNECTION_TIMEOUT"));
+      expect(xActionToCallCount.getConnectionID).toBe(1);
+      expect(isXConnectionClosed).toBe(true);
+    });
 
-    expect(xActionToCallCount.getConnectionID).toBe(1);
-    expect(isXConnectionClosed).toBe(true);
-  });
+    it("rejects on connection timeout", async () => {
+      let isXConnectionClosed = false;
 
-  test("failure: init: connection closed", async () => {
-    xAPIMockHandlers.getConnectionID = (args) =>
-      args.client.close(undefined, "something went wrong");
+      xAPIMockHandlers.onConnectionClosed = () => (isXConnectionClosed = true);
 
-    await expect(
-      XConnection.init({
-        url: xAPIURL,
-        connectionTimeout: 1_000,
-      }),
-    ).rejects.toThrow(new UWError("CONNECTION_CLOSED", "something went wrong"));
+      await expect(
+        XConnection.init({
+          url: xAPIURL,
+          connectionTimeout: 100,
+        }),
+      ).rejects.toThrow(new UWError("CONNECTION_TIMEOUT"));
 
-    expect(xActionToCallCount.getConnectionID).toBe(1);
+      expect(xActionToCallCount.getConnectionID).toBe(1);
+      expect(isXConnectionClosed).toBe(true);
+    });
+
+    it("rejects on connection closed", async () => {
+      xAPIMockHandlers.getConnectionID = (args) =>
+        args.client.close(undefined, "something went wrong");
+
+      await expect(
+        XConnection.init({
+          url: xAPIURL,
+          connectionTimeout: 1_000,
+        }),
+      ).rejects.toThrow(
+        new UWError("CONNECTION_CLOSED", "something went wrong"),
+      );
+
+      expect(xActionToCallCount.getConnectionID).toBe(1);
+    });
   });
 });
